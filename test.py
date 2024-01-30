43635c15ef411508331f6b16595be79f5db8c963
Ilya <107246761+illya-s@users.noreply.github.com>
2024-01-30 09:41:59 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
-# # from playsound import playsound
-# import winsound
+# from playsound import playsound
+import winsound
 # from os import path
 
-# file = "C:/Users/Ilya/Music/music/Адонай.mp3"
-# winsound.PlaySound(file)
+file = "C:/Users/Ilya/Music/music/Адонай.mp3"
+winsound.PlaySound(file)
 
 # playsound('C:/Users/Ilya/Music/music/Адонай.mp3')
 
-from pydub import AudioSegment
-from pydub.playback import play
+# from pydub import AudioSegment
+# from pydub.playback import play
  
-# for playing mp3 file
-song = AudioSegment.from_mp3('C:/Users/Ilya/Music/music/Адонай.mp3')
-print('playing sound using  pydub')
-play(song)
+# song = AudioSegment.from_file('C:/Users/Ilya/Music/music/Адонай.mp3')
+# # print('playing sound using  pydub')
+# play(song)
